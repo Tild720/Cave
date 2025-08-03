9ce517da4547b11717a9578526c9c73c10b3343e
Tild720 <gloveno123@gmail.com>
2025-08-03 17:44:09 +0900

    Add : Weight System, Price Setting

---
+++ src/ReplicatedStorage/Settings/Price.lua
@@ -0,0 +1,25 @@
+local Price = {
+    OrePrices = {
+        ["Stone"] = 50,
+        ["Coal"] = 200,
+        ["Copper"] = 200,
+        ["Tin"] = 250,
+        ["Iron"] = 350,
+        ["Zinc"] = 400,
+        ["Nickel"] = 450,
+        ["Aluminium"] = 500,
+        ["Silver"] = 750,
+        ["Platinum"] = 800,
+        ["Gold"] = 1000,
+        ["Diamond"] = 1500,
+        ["Emerald"] = 2500,
+        ["Ruby"] = 4000,
+        ["Carved Ruby"] = 5500,
+        ["Thunder"] = 9000,
+        ["Crystal"] = 15000,
+        ["Volcanic"] = 25000,
+        
+    },
+}
+
+return Price
